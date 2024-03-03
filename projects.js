0b89ecd910b16cde013e7b88b0466da9129a0b34
seelosjames <97702507+seelosjames@users.noreply.github.com>
2024-03-03 10:32:10 -0700

    Started Projects Section
    Figuring out flexbox/styling

---
+++ projects.js
@@ -0,0 +1,33 @@
+fetch("projects.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const projectsContainer = document.getElementById("projects");
+
+    data.forEach((project) => {
+      const projectElement = document.createElement("div");
+      projectElement.classList.add("project");
+
+      const projectImage = document.createElement("img");
+      projectImage.src = project.img_url;
+      projectImage.alt = project.name;
+
+      const projectDiv = document.createElement("div");
+
+      const projectName = document.createElement("h3");
+      projectName.textContent = project.name;
+
+      const projectDescription = document.createElement("p");
+      projectDescription.textContent = project.description;
+
+      projectDiv.appendChild(projectName);
+      projectDiv.appendChild(projectDescription);
+
+      projectElement.appendChild(projectImage);
+      projectElement.appendChild(projectDiv);
+
+      projectsContainer.appendChild(projectElement);
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading projects:", error);
+  });
